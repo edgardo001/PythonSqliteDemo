b7070164267682935ee8f336e065b2cbf92a0d44
edgardo vasquez <edgardo.a.v@gmail.com>
2016-08-21 00:57:03 -0300

    Se agrega interfaz Qt4 No funcional, falta enlazar controles

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#importo modulos de python
 import sqlite3
 import datetime
 import sys
 
+#importo modulos de terceros
+from PyQt4 import QtCore, QtGui, uic
+
 #importo mi modulos
 import db.sqlite as dbMy
+
+# Cargar nuestro archivo .ui
+form_class = uic.loadUiType("UI/mainwindow.ui")[0]
+
+class MyWindowClass(QtGui.QMainWindow, form_class):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
 
 #metodo principal
 def main():
     conn = None
     try:    
+        app = QtGui.QApplication(sys.argv)
+        MyWindow = MyWindowClass(None)
+        MyWindow.show()
+        app.exec_()
+
         conn = sqlite3.connect('data/sqliteDB.db')
         cursor = conn.cursor()    
         
